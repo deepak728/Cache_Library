e7dd6033aff86c9c0beac187659f2906e68fab4a
deepak728 <npe.deepak@gmail.com>
2022-10-01 18:09:35 +0530

    LRU cache implementation

--- Cache/LRU.cpp
+++ Cache/LRU.cpp
@@ -2,23 +2,49 @@
 using namespace std;
 
 void LRU::insert(string key,string value){
-
+    if(nCacheDAO->getCacheSize()==nList.size()){
+        evict();
+    }
+    nCacheDAO->insertEntry(key,value);
+    if(nLRUMap.find(key)==nLRUMap.end()){
+        nList.push_front(key);
+        nLRUMap[key]=nList.begin();
+    }
+    moveEntryToFront(key);
 }
 
 void LRU::remove(string key){
+    if(nLRUMap.find(key)==nLRUMap.end()){
+        cout<<"Key is not Present"<<endl;
+        nCacheDAO->removeEntry(key);
+        return; 
+    }
+    nList.erase(nLRUMap[key]);
+    nLRUMap.erase(key);
+}
 
+void LRU::moveEntryToFront(string key){
+    if(nLRUMap.find(key)==nLRUMap.end()) return;
+    nList.erase(nLRUMap[key]);
+    nList.push_front(key);
+    nLRUMap[key]=nList.begin();
 }
 
 void LRU::update(string key,string value){
-
+    moveEntryToFront(key);
+    nCacheDAO->updateEntry(key,value);
 }
 
 string LRU::getValue(string key){
-    return "";
+    moveEntryToFront(key);
+    return nCacheDAO->getValue(key);
 }
 
 void LRU::evict(){
-
+    string key = nList.back();
+    nList.pop_back();
+    nLRUMap.erase(key);
+    nCacheDAO->evictEntry(key);
 }
 
 
--- Cache/LRU.h
+++ Cache/LRU.h
@@ -8,10 +8,12 @@ class LRU:public Cache, public Evict{
     private:
         CacheDAO* nCacheDAO = new CacheDAO();
         list<string> nList;
+        unordered_map<string,list<string>::iterator> nLRUMap;
     public:
         void insert(string key,string value);
         void remove(string key);
         void evict();
         string getValue(string key);
         void update(string key,string value);
+        void moveEntryToFront(string key);
 };
